5f7deedde629b0c01d23c5085ad9b61cf34bfd82
WesMattheus <115564950+WesMattheus@users.noreply.github.com>
2022-10-18 00:07:27 -0300

    Add files via upload

---
+++ jogoMain (1).py
@@ -0,0 +1,39 @@
+from jogoConst import LIMITE
+from console import *
+import jogo
+import console
+
+def Menu():
+    print("Menu provisorio")
+    print("1 - JOGAR")
+    print("2 - CONFIGURACOES (a fazer)")
+    print("3 - INSTRUÇÕES")
+    print("4 - SAIR")
+
+def main():
+    print("Pressione Enter para começar")
+    while True:
+        if(kbhit()):
+            c = hitKey()
+            while (ord(c) != ord('1') and ord(c) != ord('2') and ord(c) != ord('3') != ord(c) != ord('4')):
+                console.clear()
+                Menu()
+
+                try: 
+                    c = hitKey()
+                except:
+                    c = "0"
+
+            if (ord(c) == ord('1')):
+                console.clear()
+                jogo.jogar()
+            if(ord(c) == ord('2')):
+                console.clear()
+                jogo.configurar()
+            if(ord(c) == ord('3')):
+                console.clear()
+                jogo.instrucao()
+            if(ord(c) == ord('4')):
+                console.clear()
+                exit(0)              
+main()
---
+++ jogoMath.py
@@ -0,0 +1,5 @@
+def solve1(A, B, x):
+    return A*x + B
+    
+def solve2(A, B, C, x):
+    return A*x**2 + B*x + C
